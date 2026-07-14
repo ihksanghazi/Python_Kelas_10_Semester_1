113d04404eefb81e552eb43c7a681db830ca7fef
naskhi <nursandyihksan97@gmail.com>
2026-07-14 14:17:31 +0700

    adding code

---
+++ Biodata.py
@@ -0,0 +1,9 @@
+full_name = input("Insert Full Name: ")
+Nick_Name = input("Input Nick Name: ")
+Age = input("Input Age: ")
+Email = input("Input Email: ")
+
+print("Full Name: ", full_name)
+print("Nick Name: ", Nick_Name)
+print("Age: ",Age)
+print("Email: ",Email)
